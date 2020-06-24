8f8c1b854838f07dc4240e46d1d468590088b928
ikonovalov <ivan.konovalov@noveogroup.com>
2020-06-24 18:35:32 +0700

    initial commit

---
+++ precache-manifest.ca62cc379032aaaafa6294b85bd353b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc2225c307dd5966470af26a80120fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "978661aa2ada513a7de9",
+    "url": "/static/css/main.1fdf9431.chunk.css"
+  },
+  {
+    "revision": "5f49d9ebf8c05411963b",
+    "url": "/static/js/2.9511c4c0.chunk.js"
+  },
+  {
+    "revision": "095637327dcea00717366af1c32b6a95",
+    "url": "/static/js/2.9511c4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978661aa2ada513a7de9",
+    "url": "/static/js/main.9958818a.chunk.js"
+  },
+  {
+    "revision": "20fc5c3b533439d3de3f",
+    "url": "/static/js/runtime-main.cfa11bc7.js"
+  }
+]);
